b899f0d309d44f299489f9f1d834606d4215c0c2
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2018-03-20 13:32:42 -0400

    server is functioning

--- springhill_frontend/server.js
+++ springhill_frontend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app     = express();
 const port    = process.env.PORT || 4000;
 
-app.use(express.static('public'));
+app.use(express.static('public/view'));
 
 app.listen(port, ()=>{
   console.log("PORT " + port + " IS FUNCTIONING.")
