cb68b4a323694b1bd9b766da94ac4a780e301e3e
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2018-04-16 17:08:33 -0400

    jumping apple in index

--- springhill_frontend/public/view/js/main.js
+++ springhill_frontend/public/view/js/main.js
@@ -1,6 +1,10 @@
 $(()=>{
-  console.log("app.js is functioning")
+  // console.log("app.js is functioning")
 
-  
+  const rollAway = () => {
+    console.log("clicked rollApple");
+    $("#rollApple").css({'animation-name':'rollOut','animation-iteration-count':'1','animation-timing-function':'linear'})
+  }
+  $("#rollApple").click(rollAway);
 
 })
