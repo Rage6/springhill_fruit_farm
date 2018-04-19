c5551aa67e15bb59fc42412b669a4200e8de04ca
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2018-04-18 21:19:57 -0400

    apple animation done

--- springhill_frontend/public/view/js/main.js
+++ springhill_frontend/public/view/js/main.js
@@ -1,10 +1,12 @@
 $(()=>{
   // console.log("app.js is functioning")
+  $("#allTopics").hide();
 
-  const rollAway = () => {
-    console.log("clicked rollApple");
-    $("#rollApple").css({'animation-name':'rollOut','animation-iteration-count':'1','animation-timing-function':'linear'})
+  const inAppleBox = (aFunction) => {
+    console.log("clicked inAppleBox function");
+    $("#rollApple").css({'animation-name':'inAppleBox','animation-duration':'1.5s','animation-iteration-count':'1','animation-timing-function':'cubic-bezier(0,.5,1,.5);'});
+    $("#allTopics").delay(1500).show(0);
   }
-  $("#rollApple").click(rollAway);
+  $("#rollApple").click(inAppleBox);
 
 })
