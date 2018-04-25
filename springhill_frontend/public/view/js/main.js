2b2040d6cc1bcb46dc256b24dddb38912abc0511
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2018-04-25 16:51:55 -0400

    fruit selectors added, start listing

--- springhill_frontend/public/view/js/main.js
+++ springhill_frontend/public/view/js/main.js
@@ -2,8 +2,40 @@ $(()=>{
 
   //Starting variables
   var menuUp = true;
+  const apples = [
+    {
+      name: "Gold Rush",
+      description: "As a hard & very tart apple, it is excellent for baking for things like pies, or simply for eating them by hand.",
+      cost: 6.00,
+      byDozen: true
+    }
+  ];
+  const pears = [
+    {
+      name: "Bosc",
+      description: "These firm and juicy pears are great for cooking, juicing, and eating by hand.",
+      cost: 5.50,
+      byDozen: true
+    }
+  ];
+  const peaches = [
+    {
+      name: "some peach",
+      description: "These firm and juicy pears are great for cooking, juicing, and eating by hand.",
+      cost: 5.50,
+      byDozen: true
+    }
+  ];
+  const pumpkins = [
+    {
+      name: "some pumpkin",
+      description: "These firm and juicy pears are great for cooking, juicing, and eating by hand.",
+      cost: 5.50,
+      byDozen: true
+    }
+  ];
 
-  // For moving the apple and revealing the buttons
+  // For throwing the apple and revealing the buttons
   $("#allTopics").hide();
   const inAppleBox = (aFunction) => {
     $("#rollApple").css({'animation-name':'inAppleBox','animation-duration':'1.5s','animation-iteration-count':'1','animation-timing-function':'cubic-bezier(0,.5,1,.5);'});
@@ -27,6 +59,44 @@ $(()=>{
     } else {
       raiseMenu();
     }
+  })
+
+  // For listing all fruit in Produce Page
+  const listFruit = (typeList) => {
+    for (var a = 0; a < typeList.length; a++) {
+      console.log(typeList[a].name);
+    }
+  };
+  listFruit(apples);
+  listFruit(pears);
+  listFruit(peaches);
+  listFruit(pumpkins);
+
+  // For selecting the fruit types
+  const unselectAll = () => {
+    $("#appleButton").css('color','white').css('background-color','transparent');
+    $("#pearButton").css('color','white').css('background-color','transparent');
+    $("#peachButton").css('color','white').css('background-color','transparent');
+    $("#pumpkinButton").css('color','white').css('background-color','transparent');
+  }
+  $("#appleButton").click(()=>{
+    unselectAll();
+    $("#appleButton").css('color','black').css('background-color','white');
+  })
+  $("#pearButton").click(()=>{
+    unselectAll();
+    $("#pearButton").css('color','black').css('background-color','white');
+  })
+  $("#peachButton").click(()=>{
+    unselectAll();
+    $("#peachButton").css('color','black').css('background-color','white');
+  })
+  $("#pumpkinButton").click(()=>{
+    unselectAll();
+    $("#pumpkinButton").css('color','black').css('background-color','white');
+  })
+  $("#allButton").click(()=>{
+    unselectAll();
   })
 
 })
